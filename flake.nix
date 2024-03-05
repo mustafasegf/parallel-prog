584a86c0dce61358fb819dfbf6bd26662504ad10
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-03-05 23:09:05 +0700

    feat: initialize simple mpi program

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "MPI devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          nativeBuildInputs = [ cmake gnumake pkg-config ];
+          buildInputs = [
+            openmpi
+            slurm
+          ];
+        };
+      }
+    );
+}
