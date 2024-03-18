e5a95c2dae6097bc0048c520c5cef44b468e3bf6
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-03-18 21:12:17 +0700

    feat: better print out and default global_sum = 0

--- mpi-process-group/main.c
+++ mpi-process-group/main.c
@@ -3,36 +3,41 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-    MPI_Init(&argc, &argv);
+  MPI_Init(&argc, &argv);
 
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Determine color based on even/odd rank for splitting
-    int color = rank % 2;
+  // Determine color based on even/odd rank for splitting
+  int color = rank % 4;
 
-    // New communicator for the subgroup
-    MPI_Comm new_comm;
-    MPI_Comm_split(MPI_COMM_WORLD, color, rank, &new_comm);
+  // New communicator for the subgroup
+  MPI_Comm new_comm;
+  MPI_Comm_split(MPI_COMM_WORLD, color, rank, &new_comm);
 
-    // Each subgroup performs a simple collective operation
-    int local_sum = rank + 1; // Just a simple operation for example
-    int global_sum;
-    MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, new_comm);
+  // Each subgroup performs a simple collective operation
+  int local_sum = rank + 1; // Just a simple operation for example
+  int global_sum = 0;
+  MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, new_comm);
 
-    // Only rank 0 in each subgroup prints the result
-    int new_rank, new_size;
-    MPI_Comm_rank(new_comm, &new_rank);
-    MPI_Comm_size(new_comm, &new_size);
-    if (new_rank == 0) {
-        printf("I am the master of my subgroup (original rank %d), subgroup size: %d, sum: %d\n", rank, new_size, global_sum);
-    }
+  // Only rank 0 in each subgroup prints the result
+  int new_rank, new_size;
+  MPI_Comm_rank(new_comm, &new_rank);
+  MPI_Comm_size(new_comm, &new_size);
+  if (new_rank == 0) {
+    printf("I am the master of my subgroup (original rank %2d), subgroup size: "
+           "%d, global_sum: %5d, local_sum: %5d\n",
+           rank, new_size, global_sum, local_sum);
+  } else {
+    printf("I am the slave  in my subgroup (original rank %2d), subgroup size: "
+           "%d, global_sum: %5d, local_sum: %5d\n",
+           rank, new_size, global_sum, local_sum);
+  }
 
-    // Free the new communicator
-    MPI_Comm_free(&new_comm);
+  // Free the new communicator
+  MPI_Comm_free(&new_comm);
 
-    MPI_Finalize();
-    return 0;
+  MPI_Finalize();
+  return 0;
 }
-
