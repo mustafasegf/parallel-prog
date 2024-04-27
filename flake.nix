328c2b97be09742a50a1354db7571bc57b95787c
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-04-27 23:23:17 +0700

    chore: added cuda on nix flakes

--- flake.nix
+++ flake.nix
@@ -11,17 +11,22 @@
       let
         pkgs = import nixpkgs {
           inherit system;
+          config.allowUnfree = true;
+          config.cudaSupport = true;
         };
-      in
-      with pkgs;
-      {
+      in with pkgs; {
         devShells.default = mkShell {
           nativeBuildInputs = [ cmake gnumake pkg-config ];
-          buildInputs = [
-            openmpi
-            slurm
-          ];
+          buildInputs = [ openmpi cudaPackages.cudatoolkit ];
+
+          packages = with pkgs; [ gcc12 ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH=/nix/store/nlw286w6v81r3imr61xkyq52wv0l20d7-zluda-3/lib:$LD_LIBRARY_PATH
+            export LD_LIBRARY_PATH=/nix/store/shr03swgapa05gsh7y0zfch2vs3xb7s7-clr-6.0.2/lib/:$LD_LIBRARY_PATH
+            # export ROCM_VISIBLE_DEVICES=GPU-a8ec4909d84b514e
+            # export HIP_VISIBLE_DEVICES=0
+          '';
         };
-      }
-    );
+      });
 }
