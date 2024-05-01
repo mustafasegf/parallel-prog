2ea70c981ebcca8eea48bc9e4b8fff16f033ec4f
Didiet Noor <lynxluna@gmail.com>
2024-05-02 04:30:31 +0700

    port: build for macOS

--- cuda-matmul/matrix.hpp
+++ cuda-matmul/matrix.hpp
@@ -3,7 +3,21 @@
 #include <sstream>
 #include <stdexcept>
 #include <vector>
+
+#if defined(__x86_64__) || defined(_M_X64)
 #include <xmmintrin.h>
+#else
+#include <cstdlib>
+static inline void *_emulate_mm_malloc(size_t size, size_t align) {
+  void *ptr = nullptr;
+  if (posix_memalign(&ptr, align, size) != 0) {
+    throw std::bad_alloc();
+  }
+  return ptr;
+}
+#define _mm_malloc _emulate_mm_malloc
+#define _mm_free free
+#endif 
 
 template <typename T> class AlignedAllocator {
 public:
--- cuda-matmul/sequential.cpp
+++ cuda-matmul/sequential.cpp
@@ -1,9 +1,13 @@
-#include "matrix.cpp"
 #include <chrono>
+
+#if defined(__x86_64__) || defined(_M_X64)
 #include <immintrin.h>
+#endif 
+
 #include <iostream>
 #include <numeric>
 #include <stdint.h>
+#include "matrix.hpp"
 
 using data_type = double;
 
