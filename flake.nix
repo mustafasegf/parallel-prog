2c89b4189bafc0bd19cb067f6b776c00439a7e22
૮༼⚆︿⚆༽つ <4953069+DrSensor@users.noreply.github.com>
2024-05-02 09:46:29 +0700

    chore: no hardcode to /nix/store/

--- flake.nix
+++ flake.nix
@@ -22,8 +22,8 @@
           packages = with pkgs; [ gcc12 ];
 
           shellHook = ''
-            export LD_LIBRARY_PATH=/nix/store/nlw286w6v81r3imr61xkyq52wv0l20d7-zluda-3/lib:$LD_LIBRARY_PATH
-            export LD_LIBRARY_PATH=/nix/store/shr03swgapa05gsh7y0zfch2vs3xb7s7-clr-6.0.2/lib/:$LD_LIBRARY_PATH
+            export LD_LIBRARY_PATH=${pkgs.zluda}/lib:$LD_LIBRARY_PATH
+            export LD_LIBRARY_PATH=${pkgs.rocmPackages.clr}/lib/:$LD_LIBRARY_PATH
             # export ROCM_VISIBLE_DEVICES=GPU-a8ec4909d84b514e
             # export HIP_VISIBLE_DEVICES=0
           '';
