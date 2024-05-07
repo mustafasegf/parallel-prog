4036a90d76ddbb3a10cca44c968043b42d78b7cb
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-05-07 19:48:10 +0700

    feat: better script for cuda simple

--- cuda-minimal/run.sh
+++ cuda-minimal/run.sh
@@ -1,25 +1,45 @@
 #!/usr/bin/env bash
-make
+# make
 
-GRID=(
-	1
-	2
-	8
-	10
+# GRID=(
+# 	1
+# 	2
+# 	8
+# 	10
+#   16
+# )
+#
+# BLOCK=(
+# 	1
+# 	2
+# 	7
+# 	8
+# 	10
+#   16
+# )
+#
+# for G in "${GRID[@]}"; do
+# 	for B in "${BLOCK[@]}"; do
+# 		echo
+# 		echo "* * * * * grid:$G block:$B"
+# 		./minimal "$G" "$B"
+# 	done
+# done
+
+GRID_BLOCK=(
+	"1 1"
+	"2 2"
+	"8 7"
+	"8 8"
+	"16 10"
 )
 
-BLOCK=(
-	1
-	2
-	7
-	8
-	10
-)
-
-for G in "${GRID[@]}"; do
-	for B in "${BLOCK[@]}"; do
+for i in "${GRID_BLOCK[@]}"; do
+	G=${i% *}
+	B=${i#* }
+	if [ "$i" != "1 1" ]; then
 		echo
-		echo "* * * * * grid:$G block:$B"
-		./minimal "$G" "$B"
-	done
+	fi
+	echo "* * * * * grid:$G block:$B"
+	./minimal "$i"
 done
